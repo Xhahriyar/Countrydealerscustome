005e0d2fe7f82f8667145328cc8ca11a84acd24e
shahriyar <shahriyardev45@gmail.com>
2024-11-10 21:53:28 +0500

    add spatie

--- resources/views/admin/client/index.blade.php
+++ resources/views/admin/client/index.blade.php
@@ -1,6 +1,19 @@
 @extends('admin.app')
 @section('content')
     <div class="content-wrapper">
+        <div class="p-6 text-gray-900">
+            {{ __("You're logged in!") }}
+            
+        </div>
+        @can('edit articles')
+            You can EDIT ARTICLES.
+        @endcan
+        @can('publish articles')
+            You can PUBLISH ARTICLES.
+        @endcan
+        @can('only super-admins can see this section')
+            Congratulations, you are a super-admin!
+        @endcan
         <div class="page-header">
             <h3 class="page-title">
                 Clients
